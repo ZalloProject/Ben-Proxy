e60796bc451c9a7a0ef44159a8ba1e4f7648a010
Ben Poling <benpoling@Bens-MacBook-Pro.local>
2019-03-30 11:33:30 -0700

    initial proxy server set up so its serving my service to the page

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path')
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, '/../public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
