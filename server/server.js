a34ce91387d2091f7d95d7aae35a5d71450880a2
Ben Poling <benpoling@Bens-MacBook-Pro.local>
2019-04-12 16:42:00 -0700

    initial styling for all of our services for desktop

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const app = require('./index.js');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
-app.listen(PORT);
+app.listen(PORT, () => console.log('listening on ' + PORT));
